dcc440201a2722fda11bdf7da65ac062fe43b6b8
hrmansso <hrmansso@ncsu.edu>
2026-08-20 11:10:00 -0400

    skeleton: initalized springboot, astro and installed tailwind.

---
+++ backend/src/test/java/com/example/resume_application_tracker/ResumeApplicationTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resume_application_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeApplicationTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
